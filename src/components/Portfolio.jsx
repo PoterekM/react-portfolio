3a51dfcf89e1d10d7ae499da6c5e889f36f15a26
Michelle Poterek <poterekm@gmail.com>
2017-09-29 16:29:28 -0700

    branching can 'toggle' form area content but does not continue to do so after hitting about again

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,20 +3,28 @@ import { connect } from "react-redux";
 import GetPortfolio from "./GetPortfolio";
 
 const Portfolio = ({ dispatch, user }) => {
-
+  let formAreaContent;
   console.log(user.userId)
+  if (user.userId === 0) {
+    formAreaContent =
+      <div>
+          <GetPortfolio/>
+      </div>
+  } else {
+    formAreaContent =
+    <div>
+      <a href="https://github.com/PoterekM">
+        <img src={user.user.avatar_url}/>
+        <h1>{user.user.login}</h1>
+        <h4>{user.user.location}</h4>
+      </a>
+    </div>
+  }
 
 
   return(
     <div>
-     <hr/>
-    <a href="https://github.com/PoterekM">
-      <img src={user.user.avatar_url}/>
-      <h1>{user.user.login}</h1>
-      <h4>{user.user.location}</h4>
-    </a>
-    <GetPortfolio/>
-    <h3>hello</h3>
+     {formAreaContent}
     </div>
   );
 }
