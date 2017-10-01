c5907f5be57be9b65db5cde7907881a2cdebab69
Michelle Poterek <poterekm@gmail.com>
2017-10-01 00:40:09 -0700

    define action as well as making second test pass

--- __tests__/reducers/index-reducer.test.js
+++ __tests__/reducers/index-reducer.test.js
@@ -8,13 +8,13 @@ describe("profile index reducer", () => {
     expect(profile([], { type: null })).toEqual([]);
   });
 
-  test('it should update state upon action to show profile', () => {
-    
+  test('it should update state upon request for profile', () => {
+    const action = actions.requestGit();
     const newUser = {
       isFetching: true,
       userId: action.userId,
     };
-    expect(profile(defaultState.profile, action) [action.userId]).toEqual(["Michelle", "turds"]);
+    expect(profile(defaultState.profile, action) [action.userId]).toEqual(newUser);
   });
 
 });
